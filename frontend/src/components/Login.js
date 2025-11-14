5bd49d007f92d7edff1dac90d6b6de08734aab13
Abayev <240918@astanait.edu.kz>
2025-11-14 21:42:32 +0500

    Fix build frontend

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,11 +1,9 @@
 import React, { useState } from 'react';
 import { login } from '../services/api';
-import { useNavigate } from 'react-router-dom';
 
 function Login() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const navigate = useNavigate();
 
     const handleSubmit = async (e) => {
         e.preventDefault();
